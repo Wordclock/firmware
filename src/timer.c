9222e5c6371a49eee72da05f86449763149ee9c5
Karol Babioch <karol@babioch.de>
2013-01-17 01:13:18 +0100

    timer.c: Add Doxygen comment block for various functions
    This removes the rather crude comments already available and replaces
    them with decent Doxygen comment blocks. Furthermore a brief description
    to various macros is added.

--- src/timer.c
+++ src/timer.c
@@ -61,15 +61,57 @@
 #include "uart.h"
 #include "wceeprom.h"
 
+/**
+ * @brief Number of interrupts per second (frequency)
+ *
+ * This defines how often per second the ISR itself will be executed. Within
+ * the ISR various functions defined in the INTERRUPT_* macros will then be
+ * called.
+ *
+ * Obviously enough this should be bigger and/or equal to the biggest
+ * INTERRUPT_* macro, namely INTERRUPT_10000HZ
+ */
 #define F_INTERRUPT           10000                                             // frequency of interrupts
 
+/**
+ * @brief List of functions that should be called 10000 times a second
+ */
 #define  INTERRUPT_10000HZ    {  irmp_ISR(); }
+/**
+ * @brief List of functions that should be called 1000 times a second
+ */
 #define  INTERRUPT_1000HZ     { }
+/**
+ * @brief List of functions that should be called 100 times a second
+ */
 #define  INTERRUPT_100HZ      { dcf77_ISR(); user_isr100Hz(); }
+/**
+ * @brief List of functions that should be called 10 times a second
+ */
 #define  INTERRUPT_10HZ       { user_isr10Hz(); display_blinkStep(); }
+/**
+ * @brief List of functions that should be called once a second
+ */
 #define  INTERRUPT_1HZ        { main_ISR(); ldr_ISR(); user_isr1Hz(); }
+/**
+ * @brief List of functions that should be called once a minute
+ */
 #define  INTERRUPT_1M         { }
 
+/**
+ * @brief Initializes the timers
+ *
+ * This function has to be called once, which will initialize the timers
+ * and - presumed that interrupts are enabled - will execute the appropriate
+ * ISRs regularly.
+ *
+ * You'll find a description of the various registers used in this function at
+ * [1], p. 130f, section 16.11.
+ *
+ * [1]: http://www.atmel.com/images/doc2545.pdf
+ *
+ * @see ISR(TIMER1_CAPT_vect)
+ */
 void
 timer_init (void)
 {
@@ -79,9 +121,29 @@ timer_init (void)
   TIMSK1  = 1 << ICIE1;                                          // ICIE1: Interrupt if timer reaches the Top (ICR1 register)
 }
 
-/*---------------------------------------------------------------------------------------------------------------------------------------------------
- * timer 1 compare handler, called every 1/10000 sec
- *---------------------------------------------------------------------------------------------------------------------------------------------------
+/**
+ * @brief Timer/Counter1 compare handler (ISR)
+ *
+ * This function is called with the frequency defined in F_INTERRUPT. It
+ * has some internal counters to divide this frequency down into the various
+ * smaller frequencies (e.g. INTERRUPT_1000HZ to INTERRUPT_1M). It then, if
+ * necessary calls the functions defined in these macros.
+ *
+ * The timer has to be initialized using timer_init() **before** this is
+ * actually executed.
+ *
+ * @warning You should make sure that the defined functions are short enough
+ * to guarantee correct timings. Otherwise some interrupts could be missed,
+ * which obviously would mess things up.
+ *
+ * @see timer_init()
+ * @see F_INTERRUPT
+ * @see INTERRUPT_10000HZ
+ * @see INTERRUPT_1000HZ
+ * @see INTERRUPT_100HZ
+ * @see INTERRUPT_10HZ
+ * @see INTERRUPT_1HZ
+ * @see INTERRUPT_1M
  */
 ISR(TIMER1_CAPT_vect)
 {
