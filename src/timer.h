fc00a26220ddad7f76eefa4d804c4626f039882f
Karol Babioch <karol@babioch.de>
2013-01-17 01:10:21 +0100

    timer.h: Refine Doxygen comment block
    This add a more detailed description to the Doxygen comment block and
    removes some redundant lines from the previous comment.

--- src/timer.h
+++ src/timer.h
@@ -18,22 +18,15 @@
  * along with Wordclock. If not, see <http://www.gnu.org/licenses/>.
  */
 
-/*------------------------------------------------------------------------------------------------------------------------------------------------*//**
+/**
  * @file timer.h
- * 
- *  Handles the timer ISRs and the providing of diffrent clock sources
+ * @brief Interface for access to the timers including the appropriate ISRs
  *
- * \version $Id: timer.h 285 2010-03-24 21:43:24Z vt $
- * 
- * \author Copyright (c) 2010 Frank Meyer - frank(at)fli4l.de
-* 
- * \remarks
- *   This program is free software; you can redistribute it and/or modify
- *   it under the terms of the GNU General Public License as published by
- *   the Free Software Foundation; either version 2 of the License, or
- *   (at your option) any later version.
+ * This module is responsible for setting up the timers of the microcontroller.
+ * It's basically enough to initialize the module by calling timer_init().
+ *
+ * @see timer.c
  */
- /*-----------------------------------------------------------------------------------------------------------------------------------------------*/
 
 #ifndef _WC_TIMER_H_
 #define _WC_TIMER_H_
