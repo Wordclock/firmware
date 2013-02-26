06f82fa019a2fd37fccd011a72e794a4506e2631
Karol Babioch <karol@babioch.de>
2013-02-26 23:02:24 +0100

    main.h: Remove comments and accommodate for coding style

--- src/main.h
+++ src/main.h
@@ -34,86 +34,49 @@
 #define _WC_MAIN_H_
 
 #ifndef F_CPU
-#error F_CPU unkown
+
+    #error F_CPU unkown
+
 #endif
 
-
-  
-/** Macro to build software version byte */
-#define BUILD_VERSION( x,y )    ( (uint8_t)((x<<4)|(y)))
+#define BUILD_VERSION(x, y) ((uint8_t)((x << 4) | (y)))
 
 #define MAJOR_VERSION 0
 #define MINOR_VERSION 12
 
-/** Defines the software version  */
-#define SW_VERSION   BUILD_VERSION(  MAJOR_VERSION, MINOR_VERSION  )
+#define SW_VERSION BUILD_VERSION(MAJOR_VERSION, MINOR_VERSION)
 
-#define WC_DISP_GER   0   /**< Word Clock with german layout with 2 regional idoms */
-#define WC_DISP_GER3  1   /**< Word Clock with german layout with 3 regional idoms */
-#define WC_DISP_ENG   0   /**< Word Clock with english layout                      */
+#define WC_DISP_GER 0
+#define WC_DISP_GER3 1
+#define WC_DISP_ENG 0
 
-/**
- *  if only one color LEDs are used.
- *  changing this to 1 will save a lot of memory
- *  by disabling all the RGB-Code
- */
 #define MONO_COLOR_CLOCK 0
 
-/**
- *  activates code for DCF control 
- *  \details
- *      If active there is also a detection if really a dcf module is connected.\n
- *      So there is no need to deactivate it. 
- *      Never the less deactivating saves ~1k so it may help using a smaller controller.
- */
-#define DCF_PRESENT           1
-#define AMBILIGHT_PRESENT     1  /**< activates Code for ambilight control GPO1 */
-#define BLUETOOTH_PRESENT     1  /**< activates Code for bluetooth control GPO2 */
-#define AUXPOWER_PRESENT      1  /**< activates Code for auxiliary control GPO3 */
-	
-/**
- *  activates code for UART Reset to start bootloader
- *  \ details
- *       If active the UART is enable to jump to the bootloader.\n
- *       Therefore just send an 'R' via UART and the bootloader will be started.\n
- *       It is possible to reset via Watchdog (BOOTLOADER_RESET_WDT = 1)
- *       or to jump direclty to the bootloader (BOOTLOADER_RESET_WDT = 0).
- */
+#define DCF_PRESENT 1
+#define AMBILIGHT_PRESENT 1
+#define BLUETOOTH_PRESENT 1
+#define AUXPOWER_PRESENT 1
+
 #define BOOTLOADER_RESET_UART 1
-#define BOOTLOADER_RESET_WDT  1
+#define BOOTLOADER_RESET_WDT 1
 
-
-/* most important user parameters */
-
-/** if '1' only the commands of activated functions (e.g. ambilight) will be 
- *  saved and trained. Also individual latex manual will be created. 
- *  Else if '0' all functions (present or not) have to be assigned in training sequence
- */
 #define INDIVIDUAL_CONFIG 1
 
-/** enables or disables auto save of parameters to eeprom 
- *  after a while without received IR-Data
- *  also see USER_DELAY_BEFORE_SAVE_EEPROM_s
- */
 #define USER_AUTOSAVE 1
 
+#define LOG_MAIN 0
+#define LOG_MAIN_BRIGHTNESS 0
+#define LOG_LDR 0
+#define LOG_LDR2PWM 0
+#define LOG_DISPLAY_STATE 0
+#define LOG_USER_IR_CMD 0
+#define LOG_USER_STATE 0
+#define LOG_USER_TIME 0
+#define LOG_USER_IR_TRAIN 0
+#define LOG_EEPROM_INIT 0
+#define LOG_EEPROM_WRITEBACK 0
+#define LOG_DCF77 0
 
-
-
-// Debugging:
-#define LOG_MAIN               0  /**< reports clock init on UART                               */
-#define LOG_MAIN_BRIGHTNESS    0  /**< reports brightnes updates to UART                        */
-#define LOG_LDR                0  /**< reports all ldr measurements to UART                     */
-#define LOG_LDR2PWM            0  /**< reports changes on ldr2pwm mapping to UART               */
-#define LOG_DISPLAY_STATE      0  /**< reports the display output data to UART                  */
-#define LOG_USER_IR_CMD        0  /**< reports the detected IR Command to UART                  */
-#define LOG_USER_STATE         0  /**< reports the internal state changes to UART               */
-#define LOG_USER_TIME          0  /**< reports internal time updates to UART                    */
-#define LOG_USER_IR_TRAIN      0  /**< reports some usefull information on IR-Training to UART  */
-#define LOG_EEPROM_INIT        0  /**< reports EEPROM data on init to UART                      */
-#define LOG_EEPROM_WRITEBACK   0  /**< reports changed EEPROM data to UART                      */
-#define LOG_DCF77              0  /**< reports various states and changes of the DCF module		*/
-
-extern void       main_ISR(void);
+extern void main_ISR(void);
 
 #endif /* _WC_MAIN_H_ */
