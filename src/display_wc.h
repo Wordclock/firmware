3f7d3d312ab8d297f167a73f9643096cedac30c8
Karol Babioch <karol@babioch.de>
2013-03-23 22:27:32 +0100

    display_wc.h: Add Doxygen comment blocks for various entities

--- src/display_wc.h
+++ src/display_wc.h
@@ -58,13 +58,55 @@
 
 #endif
 
+/**
+ * @brief Port and pin definition of the line in control of the first minute
+ *
+ * @see ports.h
+ */
 #define DISPLAY_MIN1 PORTB, 0
+
+/**
+ * @brief Port and pin definition of the line in control of the second minute
+ *
+ * @see ports.h
+ */
 #define DISPLAY_MIN2 PORTD, 7
+
+/**
+ * @brief Port and pin definition of the line in control of the three minute
+ *
+ * @see ports.h
+ */
 #define DISPLAY_MIN3 PORTC, 2
+
+/**
+ * @brief Port and pin definition of the line in control of the four minute
+ *
+ * @see ports.h
+ */
 #define DISPLAY_MIN4 PORTC, 3
 
+/**
+ * @brief Typedef defining the order in which the LEDs are connected to the
+ *   display
+ *
+ * This enumeration itself is actually defined in the language specific files,
+ * e.g. display_wc_[language].h.
+ *
+ * @see display_wc_eng.h
+ * @see display_wc_ger.h
+ * @see display_wc_ger3.h
+ */
 typedef enum e_displayWordPos e_displayWordPos;
 
+ /**
+  * @brief Returns a state that contains an indication
+  *
+  * The returned indication can be used for informing the user about any
+  * interactions. Generally speaking this concerns the minute LEDs.
+  *
+  * @return A display state with activated indicators
+  */
 static inline DisplayState display_getIndicatorMask(void)
 {
 
