34d391047074e825d292460b7712b09645f13866
Karol Babioch <karol@babioch.de>
2013-05-22 13:25:13 +0200

    display.c: Refine Doxygen comment block for file itself

--- src/display.c
+++ src/display.c
@@ -18,23 +18,21 @@
  * along with Wordclock. If not, see <http://www.gnu.org/licenses/>.
  */
 
-/*------------------------------------------------------------------------------------------------------------------------------------------------*//**
+/**
  * @file display.c
- * 
- *  Implementation of the language-independent display stuff
+ * @brief Implementation of the display module generic to all types of clock
  *
- * \version $Id: display.c 423 2012-03-20 18:43:53Z pn $
- * 
- * \author Copyright (c) 2010 Vlad Tepesch    
- * 
- * \remarks
- *   This program is free software; you can redistribute it and/or modify
- *   it under the terms of the GNU General Public License as published by
- *   the Free Software Foundation; either version 2 of the License, or
- *   (at your option) any later version.
+ * This file contains the language and type independent implementation of the
+ * display module. Among other things this includes the appropriate timer ISR
+ * responsible for the fading between display states.
+ *
+ * @note This file should be left untouched if making adaptations to other
+ * display types. Display type specific things reside in their own files, e.g.
+ * display_[displaytype].h/c.
+ *
+ * @see display_wc.c
+ * @see shift.h
  */
- /*-----------------------------------------------------------------------------------------------------------------------------------------------*/
-
 
 #include <inttypes.h>
 #include <avr/io.h>
