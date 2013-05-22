5edb6aba59c228d9c049608e678293a39d95c013
Karol Babioch <karol@babioch.de>
2013-05-22 14:01:17 +0200

    display_wc.c: Accommodate for coding style guidelines

--- src/display_wc.c
+++ src/display_wc.c
@@ -20,13 +20,13 @@
 
 /*------------------------------------------------------------------------------------------------------------------------------------------------*//**
  * @file display_wc.c
- * 
+ *
  *  Implementation of the language-independent word clock display stuff
  *
  * \version $Id: display_wc.c 405 2011-11-24 20:39:00Z sm $
- * 
- * \author Copyright (c) 2010 Vlad Tepesch    
- * 
+ *
+ * \author Copyright (c) 2010 Vlad Tepesch
+ *
  * \remarks
  *   This program is free software; you can redistribute it and/or modify
  *   it under the terms of the GNU General Public License as published by
@@ -34,7 +34,6 @@
  *   (at your option) any later version.
  */
  /*-----------------------------------------------------------------------------------------------------------------------------------------------*/
-
 
 #include <inttypes.h>
 #include <avr/io.h>
@@ -44,90 +43,117 @@
 #include "main.h"
 #include "display.h"
 #include "shift.h"
-
 #include "uart.h"
-
 #include "ports.h"
 
-
-
-void display_init (void)
+void display_init()
 {
-  shift24_init();
-  DDR(DISPLAY_MIN1) |= (1 << BIT(DISPLAY_MIN1));
-  DDR(DISPLAY_MIN2) |= (1 << BIT(DISPLAY_MIN2));
-  DDR(DISPLAY_MIN3) |= (1 << BIT(DISPLAY_MIN3));
-  DDR(DISPLAY_MIN4) |= (1 << BIT(DISPLAY_MIN4));
 
-  PORT(DISPLAY_MIN1) &= ~(1 << BIT(DISPLAY_MIN1));
-  PORT(DISPLAY_MIN2) &= ~(1 << BIT(DISPLAY_MIN2));
-  PORT(DISPLAY_MIN3) &= ~(1 << BIT(DISPLAY_MIN3));
-  PORT(DISPLAY_MIN4) &= ~(1 << BIT(DISPLAY_MIN4));
+    shift24_init();
 
-  DISPLAY_TIMER_ENABLE_INTS();
+    DDR(DISPLAY_MIN1) |= _BV(BIT(DISPLAY_MIN1));
+    DDR(DISPLAY_MIN2) |= _BV(BIT(DISPLAY_MIN2));
+    DDR(DISPLAY_MIN3) |= _BV(BIT(DISPLAY_MIN3));
+    DDR(DISPLAY_MIN4) |= _BV(BIT(DISPLAY_MIN4));
+
+    PORT(DISPLAY_MIN1) &= ~(_BV(BIT(DISPLAY_MIN1)));
+    PORT(DISPLAY_MIN2) &= ~(_BV(BIT(DISPLAY_MIN2)));
+    PORT(DISPLAY_MIN3) &= ~(_BV(BIT(DISPLAY_MIN3)));
+    PORT(DISPLAY_MIN4) &= ~(_BV(BIT(DISPLAY_MIN4)));
+
+    DISPLAY_TIMER_ENABLE_INTS();
+
 }
 
-
-/**
- * writes the given data to display
- */
 void display_outputData(DisplayState state)
 {
-  shift24_output(state);
 
-  if( state & (1L<<DWP_min1)){
-    PORT(DISPLAY_MIN1) |=  (1 << BIT(DISPLAY_MIN1));
-  }else{
-    PORT(DISPLAY_MIN1) &= ~(1 << BIT(DISPLAY_MIN1));
-  }
+    shift24_output(state);
 
-  if( state & (1L<<DWP_min2)){
-    PORT(DISPLAY_MIN2) |=  (1 << BIT(DISPLAY_MIN2));
-  }else{
-    PORT(DISPLAY_MIN2) &= ~(1 << BIT(DISPLAY_MIN2));
-  }
+    if (state & (1L << DWP_min1)) {
 
-  if( state & (1L<<DWP_min3)){
-    PORT(DISPLAY_MIN3) |=  (1 << BIT(DISPLAY_MIN3));
-  }else{
-    PORT(DISPLAY_MIN3) &= ~(1 << BIT(DISPLAY_MIN3));
-  }
+        PORT(DISPLAY_MIN1) |= _BV(BIT(DISPLAY_MIN1));
 
-  if( state & (1L<<DWP_min4)){
-    PORT(DISPLAY_MIN4) |=  (1 << BIT(DISPLAY_MIN4));
-  }else{
-    PORT(DISPLAY_MIN4) &= ~(1 << BIT(DISPLAY_MIN4));
-  }
+    } else {
 
-#if (LOG_DISPLAY_STATE==1)
-  {
-    uint8_t i;
-    uart_puts_P("Disp: ");
-    for(i=0; i<32; ++i){
-      uart_putc( '0' + (state&1));
-      state >>= 1;
+        PORT(DISPLAY_MIN1) &= ~(_BV(BIT(DISPLAY_MIN1)));
+
     }
-    uart_putc('\n');
-  }
-#endif
-}
 
+    if (state & (1L << DWP_min2)) {
+
+        PORT(DISPLAY_MIN2) |= _BV(BIT(DISPLAY_MIN2));
+
+    } else {
+
+        PORT(DISPLAY_MIN2) &= ~(_BV(BIT(DISPLAY_MIN2)));
+
+    }
+
+    if (state & (1L << DWP_min3)) {
+
+        PORT(DISPLAY_MIN3) |= _BV(BIT(DISPLAY_MIN3));
+
+    } else {
+
+        PORT(DISPLAY_MIN3) &= ~(_BV(BIT(DISPLAY_MIN3)));
+
+    }
+
+    if(state & (1L << DWP_min4)) {
+
+        PORT(DISPLAY_MIN4) |= _BV(BIT(DISPLAY_MIN4));
+
+    } else {
+
+        PORT(DISPLAY_MIN4) &= ~(1 << BIT(DISPLAY_MIN4));
+
+    }
+
+    #if (LOG_DISPLAY_STATE == 1)
+
+        uint8_t i;
+
+        uart_puts_P("Disp: ");
+
+        for (i = 0; i < 32; i++) {
+
+            uart_putc('0' + (state & 1));
+
+            state >>= 1;
+
+        }
+
+        uart_putc('\n');
+
+    #endif
+
+}
 
 void display_autoOffAnimStep1Hz(uint8_t animPreview)
 {
-   static uint8_t s_state = 0;
-   ++s_state;
-   s_state %=8;
-   if(s_state &1)
-   {
-     DisplayState state = ((DisplayState) 1) << ((s_state>>1)+DWP_MIN_LEDS_BEGIN);
-     if( animPreview ){
-       state |= display_getNumberDisplayState(2);
-     }
-     display_fadeDisplayState( state );
-   }else{
-     display_fadeDisplayState(0);
-   }
+
+    static uint8_t s_state = 0;
+
+    s_state++;
+    s_state %= 8;
+
+    if (s_state & 1) {
+
+        DisplayState state = ((DisplayState)1) << ((s_state >> 1) + DWP_MIN_LEDS_BEGIN);
+
+        if (animPreview) {
+
+            state |= display_getNumberDisplayState(2);
+
+        }
+
+        display_fadeDisplayState(state);
+
+    } else {
+
+        display_fadeDisplayState(0);
+
+    }
+
 }
-
-
