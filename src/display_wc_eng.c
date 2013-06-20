f4510619128ebaa7bc3939d8671f33024496ef18
Karol Babioch <karol@babioch.de>
2013-06-20 21:28:08 +0200

    display_wc_eng.c: Add some notes commenting the code itself

--- src/display_wc_eng.c
+++ src/display_wc_eng.c
@@ -102,71 +102,147 @@
     DisplayState display_getTimeState(const datetime_t* i_newDateTime)
     {
 
+        /*
+         * Get a local copy of hour and minutes
+         */
         uint8_t hour = i_newDateTime->hh;
         uint8_t minutes = i_newDateTime->mm;
+
+        /*
+         * Get the number of minutes past the current "five-minute" block.
+         * This will later on be used to setup the minute LEDs
+         */
         uint8_t minuteLeds = minutes % 5;
+
+        /*
+         * Get the current "five-minute" block
+         */
         minutes = minutes / 5;
+
+        /*
+         * The variable which will be returned later on
+         */
         DisplayState leds;
 
+        /*
+         * Check whether code for deactivating the phrase "it is" should be
+         * compiled in.
+         */
         #if (DISPLAY_DEACTIVATABLE_ITIS == 1)
 
+            /*
+             * Initialize the display state
+             */
             leds = 0;
 
+            /*
+             * Check whether the phrase "it is" should actually be
+             * displayed. This is the case when either the mode is even or
+             * when the hour is "full".
+             */
             if (((g_displayParams->mode & 1) == 0) || (minutes == 0)) {
 
                 leds |= ((DisplayState)1 << DWP_itis);
 
             }
 
+        /*
+         * The phrase "it is" should always be enabled and is not
+         * deactivatable.
+         */
         #else
 
+            /*
+             * Enable the phrase "it is"
+             */
             leds = ((DisplayState)1 << DWP_itis);
 
         #endif
 
+        /*
+         * Check whether it is p.m.: In case the hour is bigger than twelve
+         * it needs to be decremented by twelve, as only hours between one and
+         * twelve can be displayed.
+         */
         if (hour > 12) {
 
             hour -= 12;
 
         }
 
+        /*
+         * Zero equals twelve
+         */
         if (hour == 0) {
 
             hour = 12;
 
         }
 
+        /*
+         * Check whether the hour is "full" or whether the time has actually
+         * progressed so far that a "five-minute" block needs to be displayed.
+         */
         if (minutes > 0) {
 
+            /*
+             * Holding the "state" for the current "five-minute" block
+             */
             uint8_t minState;
             minState = minData[minutes - 1];
 
+            /*
+             * Check whether time should be displayed relative to the next
+             * hour, e.g. "9:45" would be "quarter to ten".
+             */
             if (minutes > 6) {
 
                 ++hour;
 
             }
 
+            /*
+             * Add minState to the result to be returned
+             */
             leds |= ((DisplayState)minState) << DWP_MIN_FIRST;
 
+        /*
+         * The hour is "full" and no "five-minute" block needs to be
+         * displayed, e.g. 9:00 - 9:04
+         */
         } else {
 
+            /*
+             * Activate the phrase "clock"
+             */
             leds |= ((DisplayState)1 << DWP_clock);
 
         }
 
+        /*
+         * Iteratove over minute LEDs and enable them if necessary
+         */
         for (; minuteLeds; --minuteLeds) {
 
             leds |= ((DisplayState)1 << (minuteLeds - 1 + DWP_MIN_LEDS_BEGIN));
 
         }
 
+        /*
+         * Once again check whether the hour is bigger than twelve and
+         * decrement it if necessary, as we potentially have increased the hour
+         * in the previous passage when displaying the time relative to the
+         * next hour, e.g. "quarter to ten".
+         */
         if (hour > 12) {
 
             hour -= 12;
 
         }
 
+        /*
+         * Enable the LED group corresponding to the hour
+         */
         leds |= ((DisplayState)1 << (DWP_HOUR_BEGIN - 1 + hour));
 
         return leds;
