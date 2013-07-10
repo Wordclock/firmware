Wordclock/firmware

commit f992e20956d71e69015f5e26ee00eba2b461de0b

uart.c: Make use of <util/setbaud.h> instead of calculating values manually
<util/setbaud.h> is helper for baud rate calculations provided by avr-libc,
see [1] for details.

[1]: http://www.nongnu.org/avr-libc/user-manual/group__util__setbaud.html

src/uart.c
@@ -45,16 +45,14 @@
 #include <avr/interrupt.h>
 #endif
 
-#define UART_BAUD             9600L
+/**
+ * @brief The baud rate used for communication
+ *
+ * @see uart_init()
+ */
+#define BAUD 9600
 
-// calculate real baud rate:
-#define UBRR_VAL              ((F_CPU+UART_BAUD*8)/(UART_BAUD*16)-1)            // round
-#define BAUD_REAL             (F_CPU/(16*(UBRR_VAL+1)))                         // real baudrate
-#define BAUD_ERROR            ((BAUD_REAL*1000)/UART_BAUD)                      // error in promille
-
-#if ((BAUD_ERROR < 990) || (BAUD_ERROR > 1010))
-#  error Error of baud rate of RS232 UART is more than 1%. That is too high!
-#endif
+#include <util/setbaud.h>
 
 /**
  * @brief Initializes the UART hardware
@@ -75,8 +73,11 @@ uart_init (void)
 #else
   UCSR0B |= (1<<TXEN0);                                                         // activate UART0 TX
 #endif
-  UBRR0H = UBRR_VAL >> 8;                                                       // store baudrate (upper byte)
-  UBRR0L = UBRR_VAL & 0xFF;                                                     // store baudrate (lower byte)
+  UBRR0H = UBRR_VALUE;                                                          // store baudrate (upper byte)
+  UBRR0L = UBRR_VALUE;                                                          // store baudrate (lower byte)
+#if USE_2X
+  UCSR0A = _BV(U2X);
+#endif
 }
 
 /**

commit 6eedb26f21112bec9af4f91fdd550d99c927a2a9

uart.c: Fix an issue with wrong values being assigned to the UBRR0 register
f992e20956d71e69015f5e26ee00eba2b461de0b introduced a bug, so that the wrong
values were assigned to the UBRR registers, which rendered the UART useless.

The error was that the calculated value for the UBRR (16-bit) was assigned to
both the upper (UBRR0H) as well as the lower (UBRR0L) bytes. This fixes it by
simply assigning the calculated value (UBRR_VALUE) directly to the UBRR0
register.

src/uart.c
@@ -102,8 +102,7 @@ void uart_init()
     /*
      * Set baud rate according to calculated value
      */
-    UBRR0H = UBRR_VALUE;
-    UBRR0L = UBRR_VALUE;
+    UBRR0 = UBRR_VALUE;
 
     /*
      * Check whether speed should be doubled