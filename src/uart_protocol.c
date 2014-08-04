1c91335fcff83fc3ed26c169ae0f40b515fad37f
Karol Babioch <karol@babioch.de>
2014-08-04 23:30:15 +0200

    src: uart_protocol.c: uart_protocol_handle(): Use strncmp() correctly"
    Rather than using strlen() as argument for strncmp() use the buffer size.

--- src/uart_protocol.c
+++ src/uart_protocol.c
@@ -1107,7 +1107,7 @@ void uart_protocol_handle()
                 for (uint8_t i = 0; i < j; i++) {
 
                     bool compare = strncmp(uart_protocol_commands[i].command,
-                        argv[0], strlen(uart_protocol_commands[i].command));
+                        argv[0], UART_PROTOCOL_COMMAND_BUFFER_SIZE);
 
                     if (compare == 0
                         && (argc - 1) == uart_protocol_commands[i].arguments) {
