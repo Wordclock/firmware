f55626219c1ff938dc6cd9f581ee0fa22e88be44
Karol Babioch <karol@babioch.de>
2014-09-03 00:09:36 +0200

    src: uart_protocol: Add support for dynamic logging

--- src/uart_protocol.c
+++ src/uart_protocol.c
@@ -42,6 +42,7 @@
 #include "datetime.h"
 #include "i2c_rtc.h"
 #include "ldr.h"
+#include "log.h"
 #include "uart.h"
 #include "uart_protocol.h"
 #include "user.h"
@@ -920,6 +921,81 @@ static void _date_set(uint8_t argc, char* argv[])
 }
 
 /**
+ * @brief Enables the logging globally
+ *
+ * @see uart_protocol_command_callback_t
+ * @see log_enable()
+ * @see uart_protocol_ok()
+ */
+static void _log_enable(uint8_t argc, char* argv[])
+{
+
+    log_enable();
+    uart_protocol_ok();
+
+}
+
+/**
+ * @brief Disables the logging globally
+ *
+ * @see uart_protocol_command_callback_t
+ * @see log_disable()
+ * @see uart_protocol_ok()
+ */
+static void _log_disable(uint8_t argc, char* argv[])
+{
+
+    log_disable();
+    uart_protocol_ok();
+
+}
+
+/**
+ * @brief Sets the logging level for a specific module
+ *
+ * Both, the module and the level are expected to be passed in as hex
+ * arguments. In case the arguments are invalid, uart_protocol_error() is
+ * invoked, otherwise uart_protocol_ok() indicates success.
+ *
+ * @see uart_protocol_command_callback_t
+ * @see log_module_t
+ * @see log_level_t
+ * @see hexStrToUint8()
+ * @see log_set_level()
+ * @see uart_protocl_error()
+ * @see uart_protocol_ok()
+ */
+static void _log_set_level(uint8_t argc, char* argv[])
+{
+
+    bool status;
+
+    log_module_t module = hexStrToUint8(argv[1], &status);
+
+    if (!status || module >= LOG_MODULE_COUNT) {
+
+        uart_protocol_error();
+
+        return;
+
+    }
+
+    log_level_t level = hexStrToUint8(argv[2], &status);
+
+    if (!status || level >= LOG_LEVEL_COUNT) {
+
+        uart_protocol_error();
+
+        return;
+
+    }
+
+    log_set_level(module, level);
+    uart_protocol_ok();
+
+}
+
+/**
  * @brief Defines the type of each entry within #uart_protocol_commands
  *
  * @see uart_protocol_commands
@@ -999,6 +1075,10 @@ static uart_protocol_command_t uart_protocol_commands[] = {
     {"ts", 3, _time_set},
     {"dg", 0, _date_get},
     {"ds", 4, _date_set},
+
+    {"le", 0, _log_enable},
+    {"ld", 0, _log_disable},
+    {"ls", 2, _log_set_level},
 
 };
 
